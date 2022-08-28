95f1592ad93d87f8c4ef924e56b262a3309c6acf
Lucas Reis <lucasreis_9@hotmail.com>
2022-08-28 20:34:24 -0300

    Finalizada a seção Feture

--- js/script.js
+++ js/script.js
@@ -25,5 +25,13 @@ $(document).ready(function(){
         loop:true
     });
 
+    $('.small-image img').click(function(){
+        $(this).addClass('image-active').siblings().removeClass('image-active');
+
+        let image = $(this).attr('src');
+
+        $('.big-image img').attr('src',image);
+    });
+
 })
 
