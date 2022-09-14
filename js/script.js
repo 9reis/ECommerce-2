b784193d12ee60b45ff7fce796c138c52fde1e33
Lucas Reis <lucasreis_9@hotmail.com>
2022-09-14 20:25:20 -0300

    Finalizada a seção Gallery utilizando filtro em JQuery

--- js/script.js
+++ js/script.js
@@ -14,7 +14,9 @@ $(document).ready(function(){
         }else {
             $('header .header-2').removeClass('header-active');
         }
+
     })
+    
     
     $('.home-slider').owlCarousel({
         items:1,
@@ -27,11 +29,27 @@ $(document).ready(function(){
 
     $('.small-image img').click(function(){
         $(this).addClass('image-active').siblings().removeClass('image-active');
-
         let image = $(this).attr('src');
-
         $('.big-image img').attr('src',image);
     });
+
+    $('.gallery .btn').click(function(){
+        let filter = $(this).attr('data-filter')
+
+        if(filter == 'all'){
+            $('.gallery .box').show(400)
+        }else {
+            $('.gallery .box').not('.'+filter).hide(200)
+            $('.gallery .box').filter('.'+filter).show(400)
+        }
+
+        $(this).addClass('button-active').siblings().removeClass('button-active')
+
+    })
+
+    
+   
+
 
 })
 
