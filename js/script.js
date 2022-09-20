6504eabd86d8feb498119de801b8b10bb7915aee
Lucas Reis <lucasreis_9@hotmail.com>
2022-09-20 19:04:25 -0300

    Ajuste do menu mobile

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+
 $(document).ready(function(){
 
     $('#menu').click(function(){
-        $(this).toggleClass('fa-times')
-        $('.navbar').toggleClass('nav-toogle')
-    })
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
 
-    $(window).on('scroll load', function(){
+    $(window).on('scroll load',function(){
+
         $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toogle')
+        $('.navbar').removeClass('nav-toggle');
 
         if($(window).scrollTop() > 68){
             $("header .header-2").addClass('header-active');
